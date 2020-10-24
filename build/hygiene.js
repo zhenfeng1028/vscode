9e33122da6715a8af761321bd2930539d0e7e879
Matt Bierner <matb@microsoft.com>
2020-10-23 19:14:14 -0700

    Add simple browser extension
    This change adds a new 'simple browser' extension. This extension uses a webview to render webpages directly in VS Code. We plan on using it for optionally previewing local servers in both desktop and codespaces
    
    The browser itself has a number of limitations due to the security around iframes:
    
    - It traps keyboard focus
    - We can't detect if a page fails to load
    - We can't track the current url of the iframe

--- build/hygiene.js
+++ build/hygiene.js
@@ -91,6 +91,7 @@ const indentationFilter = [
 	'!**/*.Dockerfile',
 	'!**/*.dockerfile',
 	'!extensions/markdown-language-features/media/*.js',
+	'!extensions/simple-browser/media/*.js',
 ];
 
 const copyrightFilter = [
